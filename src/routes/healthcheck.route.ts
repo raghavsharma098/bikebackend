0882aa81d11aaea9ffd121b78fb76a350a93a2ac
raghavsharma098 <raghavrock098@gmail.com>
2025-09-23 00:45:33 +0530

    done

--- src/routes/healthcheck.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import checkHealth from "../controllers/healthcheck.controller";
-
-const router = Router();
-
-router.route("/").get(checkHealth);
-
-export default router;
